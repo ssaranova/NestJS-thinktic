2de3da69c07aed3b5587fe7dc85524de107264ff
Saúl Saranova <ssaranovas@gmail.com>
2020-03-26 20:43:56 +0100

    Second exercise
    Libro service get all database items

--- ejercicionest/src/api/libro/services/libro.service.ts
+++ ejercicionest/src/api/libro/services/libro.service.ts
@@ -1,4 +1,15 @@
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+
 import { Injectable } from '@nestjs/common';
+import { Libro } from '../interfaces/libro-mongo';
 
 @Injectable()
-export class LibroService {}
+export class LibroService {
+
+    constructor(@InjectModel('Libro') private readonly bbdd: Model<Libro>) { }
+
+    async getLibros(): Promise<Libro[]> {
+        return await this.bbdd.find().exec();
+    }
+}
