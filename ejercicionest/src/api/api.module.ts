ed27a011ee9be5e4a4686189964ace46fa3d3d78
Saúl Saranova <ssaranovas@gmail.com>
2020-03-26 20:11:31 +0100

    Second exercise
    Import Libro Schema in api module

--- ejercicionest/src/api/api.module.ts
+++ ejercicionest/src/api/api.module.ts
@@ -2,8 +2,17 @@ import { Module } from '@nestjs/common';
 import { LibroController } from './libro/controllers/libro.controller';
 import { RestService } from './rest/services/rest.service';
 
+import { MongooseModule } from '@nestjs/mongoose';
+import { LibroSchema } from './libro/schemas/libro.schema';
+
 @Module({
   controllers: [LibroController],
-  providers: [RestService]
+  providers: [RestService],
+  imports: [
+    MongooseModule.forFeature([{
+      name: 'Libro',
+      schema: LibroSchema }]
+    )
+  ],
 })
 export class ApiModule {}
