5792e8648a3218c2622fb9c269b3df0154f56d41
Saúl Saranova <ssaranovas@gmail.com>
2020-03-26 19:55:07 +0100

    Second exercise
    Mongoose installation

--- ejercicionest/src/app.module.ts
+++ ejercicionest/src/app.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApiModule } from './api/api.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ApiModule],
+  imports: [
+    ApiModule,
+    MongooseModule.forRoot(
+      'mongodb://localhost/libros',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      }
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
